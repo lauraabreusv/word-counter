b9a6cc70a505108df5ccac028c31a3a4d1c15a2f
lauraabreusv <lauraabreusv@gmail.com>
2022-07-18 21:39:14 -0300

    cleaning

--- polls/count_words.py
+++ polls/count_words.py
@@ -4,9 +4,5 @@ import sys
 
 def count_words(text):
     tokenizer = RegexpTokenizer(r'\w+')
-    try:
-        c = len(tokenizer.tokenize(text))
-    except:
-        c = 0
-    return c
+    return len(tokenizer.tokenize(text))
 
